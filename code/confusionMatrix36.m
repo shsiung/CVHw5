fab6d3fb42cbfd2bef56a8e7b34548ce7c4ae38d
shsiung <shsiung@g.hmc.edu>
2016-11-21 12:09:51 -0500

    plotting confusion matrix

---
+++ code/confusionMatrix36.m
@@ -0,0 +1,22 @@
+function confusionMatrix36()
+
+classes = 26;
+layers = [32*32, 400, classes];
+
+load('../data/nist26_test.mat', 'test_data', 'test_labels')
+% load('nist26_model.mat','W','b');
+
+test_data = normalize(test_data);
+% [outputs] = Classify(W, b, test_data);
+
+test_labels =  mat2cell(test_labels,ones(1,size(test_labels,1)),size(test_labels,2));
+group_known = cellfun(@(x) find(x==max(x)),test_labels);
+outputs =  mat2cell(test_labels,ones(1,size(test_labels,1)),size(test_labels,2));
+group_predict = cellfun(@(x) find(x==max(x)),test_labels);
+
+[C,~] = confusionmat(group_known, group_predict);
+imagesc(C)
+colorbar
+
+
+end
