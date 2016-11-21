37362800bc6574268bdb1ff5ed5571231bf2e623
shsiung <shsiung@g.hmc.edu>
2016-11-21 13:43:22 -0500

    graph confusion matrix done

--- code/confusionMatrix.m
+++ code/confusionMatrix.m
@@ -4,19 +4,27 @@ classes = 26;
 layers = [32*32, 400, classes];
 
 load('../data/nist26_test.mat', 'test_data', 'test_labels')
-% load('nist26_model.mat','W','b');
+load('nist26_model.mat','W','b');
 
 test_data = normalize(test_data);
-% [outputs] = Classify(W, b, test_data);
+[outputs] = Classify(W, b, test_data);
 
 test_labels =  mat2cell(test_labels,ones(1,size(test_labels,1)),size(test_labels,2));
 group_known = cellfun(@(x) find(x==max(x)),test_labels);
-outputs =  mat2cell(test_labels,ones(1,size(test_labels,1)),size(test_labels,2));
-group_predict = cellfun(@(x) find(x==max(x)),test_labels);
+outputs =  mat2cell(outputs,ones(1,size(outputs,1)),size(outputs,2));
+group_predict = cellfun(@(x) find(x==max(x)),outputs);
 
 [C,~] = confusionmat(group_known, group_predict);
 imagesc(C)
 colorbar
-
+xticks([1:classes]);
+xticklabels({'A','B','C','D','E','F','G','H','I','J','K',...
+             'L','M','N','O','P','Q','R','S','T','U','V',...
+             'W','X','Y','Z'});
+yticks([1:classes]);
+yticklabels({'A','B','C','D','E','F','G','H','I','J','K',...
+             'L','M','N','O','P','Q','R','S','T','U','V',...
+             'W','X','Y','Z'});
+set(gca,'XAxisLocation','top','YAxisLocation','left','ydir','reverse');
 
 end
--- code/confusionMatrix36.m
+++ code/confusionMatrix36.m
@@ -1,22 +1,30 @@
 function confusionMatrix36()
+classes = 36;
+layers = [32*32, 800, classes];
 
-classes = 26;
-layers = [32*32, 400, classes];
-
-load('../data/nist26_test.mat', 'test_data', 'test_labels')
-% load('nist26_model.mat','W','b');
+load('../data/nist36_test.mat', 'test_data', 'test_labels')
+load('nist36_model.mat','W','b');
 
 test_data = normalize(test_data);
-% [outputs] = Classify(W, b, test_data);
+[outputs] = Classify(W, b, test_data);
 
 test_labels =  mat2cell(test_labels,ones(1,size(test_labels,1)),size(test_labels,2));
 group_known = cellfun(@(x) find(x==max(x)),test_labels);
-outputs =  mat2cell(test_labels,ones(1,size(test_labels,1)),size(test_labels,2));
-group_predict = cellfun(@(x) find(x==max(x)),test_labels);
+outputs =  mat2cell(outputs,ones(1,size(outputs,1)),size(outputs,2));
+group_predict = cellfun(@(x) find(x==max(x)),outputs);
 
 [C,~] = confusionmat(group_known, group_predict);
-imagesc(C)
+imagesc(C);
 colorbar
-
-
+xticks([1:36]);
+xticklabels({'A','B','C','D','E','F','G','H','I','J','K',...
+             'L','M','N','O','P','Q','R','S','T','U','V',...
+             'W','X','Y','Z','0','1','2','3','4','5','6','7',...
+             '8','9'});
+yticks([1:36]);
+yticklabels({'A','B','C','D','E','F','G','H','I','J','K',...
+             'L','M','N','O','P','Q','R','S','T','U','V',...
+             'W','X','Y','Z','0','1','2','3','4','5','6','7',...
+             '8','9'});
+set(gca,'XAxisLocation','top','YAxisLocation','left','ydir','reverse');
 end
