0a0c726bb9f5c792a9857f5e451163085e2663cf
shsiung <shsiung@g.hmc.edu>
2016-11-21 21:54:24 -0500

    working but bad accuracy

--- code/extractImageText.m
+++ code/extractImageText.m
@@ -1,12 +1,39 @@
 function [text] = extractImageText(fname)
 % [text] = extractImageText(fname) loads the image specified by the path 'fname'
-% and returns the next contained in the image as a string.
+% and returns the text contained in the image as a string.
+PLOT = 0;
 
+% Loading W and b from 3.2.1
+load('nist36_model.mat','W','b');
+letter_bank = {'A','B','C','D','E','F','G','H','I','J','K',...
+             'L','M','N','O','P','Q','R','S','T','U','V',...
+             'W','X','Y','Z','0','1','2','3','4','5','6','7',...
+             '8','9'};
+% Read the image
 im = imread(fname);
 [lines,bw] = findLetters(im);
 
+% Put the data together
+test_data = [];
 for i = 1 : length(lines)
-   
+   test_line = zeros(length(lines{i}),1024);
+   for j = 1 : length(lines{i})
+       letter = lines{i}(j,:);
+       % Find the correct letter and resize to 32*32
+       im_letter = bw(letter(2):letter(4),letter(1):letter(3));
+       im_letter = imresize(im_letter,[32 32]);
+       test_line(j,:) = reshape(im_letter,[1,1024]);
+       if (PLOT)
+        figure;
+        imshow(im_letter);
+       end
+   end
+   test_data = [test_data; test_line];
 end
+size(test_data)
+[outputs] = Classify(W, b, test_data);
+outputs = mat2cell(outputs,ones(size(outputs,1),1),size(outputs,2));
+group_predict = cellfun(@(x) find(x==max(x)),outputs);
+text = letter_bank(group_predict);
 
 end
--- code/testFindletters.m
+++ code/testFindletters.m
@@ -1,10 +1,14 @@
+function testFindletters()
 % Strings must be the same length...
 file_name = ['../images/01_list.jpg   ';...
              '../images/02_letters.jpg';...
              '../images/03_haiku.jpg  ';...
              '../images/04_deep.jpg   '];
 file_name = cellstr(file_name);
-text = cell(length(file_name),1);
 for i = 1 : length(file_name)
-text{i} = extractImageText(file_name{i});
+im = imread(file_name{i});
+figure
+findLetters(im);
+end
+
 end
