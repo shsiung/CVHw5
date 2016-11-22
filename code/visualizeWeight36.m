bc633e461c6d5be93d877558d54745226a175b59
shsiung <shsiung@g.hmc.edu>
2016-11-21 19:24:19 -0500

    fix visualize36

--- code/visualizeWeight36.m
+++ code/visualizeWeight36.m
@@ -2,6 +2,17 @@ function [test_acc, test_loss] = visualizeWeight()
 classes = 36;
 layers = [32*32, 800, classes];
 
+load('../data/nist26_model_60iters.mat','W','b');
+%% Plot initial weight
+layer_one = mat2cell(W{1},ones(1,size(W{1},1)),size(W{1},2));
+I = zeros(32,32,1,length(layer_one));
+for k = 1:length(layer_one)
+    unit = layer_one{k};
+    unit = (unit - min(unit))/(max(unit)-min(unit));
+    I(:,:,1,k) = reshape(unit,32,32); 
+end
+figure(2)
+montage(I);
 load('../data/nist36_test.mat', 'test_data', 'test_labels')
 load('nist36_model.mat','W','b');
 
@@ -16,19 +27,9 @@ for k = 1:length(layer_one)
     unit = (unit - min(unit))/(max(unit)-min(unit));
     II(:,:,1,k) = reshape(unit,32,32); 
 end
-figure(2)
-montage(II);
-%% Plot initial weight
-[W1, ~] = InitializeNetwork(layers);
-layer_one = mat2cell(W1{1},ones(1,size(W1{1},1)),size(W1{1},2));
-I = zeros(32,32,1,length(layer_one));
-for k = 1:length(layer_one)
-    unit = layer_one{k};
-    unit = (unit - min(unit))/(max(unit)-min(unit));
-    I(:,:,1,k) = reshape(unit,32,32); 
-end
 figure(1)
-montage(I);
+montage(II);
+
 
 
 end
